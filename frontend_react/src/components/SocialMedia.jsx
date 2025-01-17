23cb4cfb48c81164f3104d7390fb693f71fdd7a8
Rameswar Panda <rameswarpanda891@gmail.com>
2025-01-17 23:47:19 +0530

    bvb

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BsLinkedin, BsGithub, BsTwitterX} from 'react-icons/bs';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+    <BsLinkedin />
+    </div>
+    <div>
+    <BsGithub />
+    </div>
+    <div>
+    <BsTwitterX />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
